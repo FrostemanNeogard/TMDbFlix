57731546546ae91bf6ef188648d011b6ce870eb1
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-02-26 01:15:32 +0100

    Add failsafe for missing movie data

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -1,35 +1,75 @@
 import { useLocation } from "react-router-dom"
 import GetBackdropImage from '../utils/js/GetBackdropImage'
-import { useState } from 'react'
+import GetPosterImage from '../utils/js/GetPosterImage'
+import { useState, useEffect } from 'react'
 import GetAPIRequestURL from "../utils/js/GetAPIRequestURL"
+import GetURLParameters from "../utils/js/GetURLParameters"
 
 function MoviePage() {
   const location = useLocation()
-  let [movie, setMovie] = useState(location.state.movie)
+  let [movieDetails, setMovie] = useState(location.state?.movie)
 
   // If movie is not defined, fetch data from movie in url parameter
-  if (!movie) {
-    // Workaround to get url params since it otherwise behaves weirdly with HashRouter
-    const URL = window.location.href
-    const urlParams = URL.substring(URL.indexOf('?'), URL.length)
-    const params = new URLSearchParams(urlParams)
-    const movieId = params.get('id')
-
-    const requestUrl = GetAPIRequestURL('movie/'+movieId)
-    fetch(requestUrl)
-    .then(res => res.json())
-    .then(data => setMovie(data))
-  }
+  useEffect(() => {
+    if (!movieDetails) {
+      const params = GetURLParameters()
+      const movieId = params.get('id')
   
-
+      if (movieId == 'undefined') return
   
-  const backdropSrc = GetBackdropImage(movie.backdrop_path)
+      const requestUrl = GetAPIRequestURL('movie/'+movieId)
+      fetch(requestUrl)
+      .then(res => res.json())
+      .then(data => {
+        if (!data.success) return
+        setMovie(data)
+      })
+    }
+  }, [])
 
   return (
     <div className="single-movie-details">
-      <h1>Movie Page</h1>
-      <img src={backdropSrc} alt="" />
+      <div className="movie-information">
+        {!movieDetails ? <NoMovie /> : <MovieInformation movieDetails={movieDetails} />}
+      </div>
     </div>
+  )
+}
+
+function NoMovie() {
+  return (
+    <h1>
+      Error: this movie cannot be found.
+    </h1>
+  )
+}
+
+function MovieInformation(props) {
+
+  const { backdrop_path, genre_ids, overview, poster_path, release_date, title, vote_average } = props.movieDetails
+  const releaseYear = release_date.substring(0, 4)
+  const backdropSrc = GetBackdropImage(backdrop_path)
+  const posterSrc = GetPosterImage(poster_path)
+  const formattedReviewScore = (vote_average*10 + '%')
+
+  return (
+    <>
+      <div className="details">
+        <img src={posterSrc} alt={title} />
+        <div>
+          <div className="overview">
+            <h1>{title} <span>({releaseYear})</span></h1>
+          </div>
+          <div className="review">
+            <h3 className="review-score">{formattedReviewScore}</h3>
+          </div>
+        </div>
+      </div>
+      <div className="overview">
+        <h1>Overview</h1>
+        <p>{overview}</p>
+      </div>
+    </>
   )
 }
 
